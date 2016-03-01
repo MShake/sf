94fe06339a5b004782bcb98160cc818201662f6f
Florian <laforest.florian@gmail.com>
2016-03-01 22:13:40 +0100

    Modification des fixtures messages

--- src/AppBundle/DataFixtures/ORM/LoadGroupFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadGroupFixtures.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: lionel
  * Date: 01/03/2016
  * Time: 18:33
  */
-
 namespace AppBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
@@ -14,38 +14,34 @@ use AppBundle\Entity\ChatGroup;
 use AppBundle\Entity\User;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 
-
-class LoadGroupFixtures extends AbstractFixture implements OrderedFixtureInterface
-{
-
-    function load(ObjectManager $manager)
-        {
-
-            $i = 0;
-
-            while($i < 2) {
-                $chatGroup = new ChatGroup();
-                $chatGroup->setName("Groupe numero ".($i+1));
-				
-                if($i == 0){
-	                $chatGroup->addUser($this->getReference("users0"));
-	                $chatGroup->addUser($this->getReference("users1"));
-	                $chatGroup->addUser($this->getReference("users2"));
-                }else{
-                	$chatGroup->addUser($this->getReference("users0"));
-                	$chatGroup->addUser($this->getReference("users3"));
-                }
-            
-                $manager->persist($chatGroup);
-                $i++;
-            }
-            $manager->flush();
-        }
-
-    public function getOrder()
-    {
-        // the order in which fixtures will be loaded
-        // the lower the number, the sooner that this fixture is loaded
-        return 2;
-    }
+class LoadGroupFixtures extends AbstractFixture implements OrderedFixtureInterface {
+	
+	function load(ObjectManager $manager) {
+		$i = 0;
+		
+		while ( $i < 2 ) {
+			$chatGroup = new ChatGroup ();
+			$chatGroup->setName ( "Groupe numero " . ($i + 1) );
+			
+			if ($i == 0) {
+				$chatGroup->addUser ( $this->getReference ( "users0" ) );
+				$chatGroup->addUser ( $this->getReference ( "users1" ) );
+				$chatGroup->addUser ( $this->getReference ( "users2" ) );
+			} else {
+				$chatGroup->addUser ( $this->getReference ( "users0" ) );
+				$chatGroup->addUser ( $this->getReference ( "users3" ) );
+			}
+			
+			$this->addReference ( 'groups' . $i, $chatGroup );
+			$manager->persist ( $chatGroup );
+			$i ++;
+		}
+		$manager->flush ();
+	}
+	
+	public function getOrder() {
+		// the order in which fixtures will be loaded
+		// the lower the number, the sooner that this fixture is loaded
+		return 2;
+	}
 }
