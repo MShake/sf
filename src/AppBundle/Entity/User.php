5e8d414168b9155533901a74a490f1a9f9fa10c3
Florian <laforest.florian@gmail.com>
2016-03-01 16:06:44 +0100

    Génération des entités (User, ChatGroup, Message)

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+    /**
+     * @var string
+     * 
+     * @ORM\Column(name="firstname", type="string", length=150)
+     */
+    private $firstname;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
