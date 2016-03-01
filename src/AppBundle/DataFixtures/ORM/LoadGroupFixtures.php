54f79fbf19a69daf7889159f69af69485e914030
lionel95200x <lionel95200@gmail.com>
2016-03-01 19:03:38 +0100

    Fixture chat Group

---
+++ src/AppBundle/DataFixtures/ORM/LoadGroupFixtures.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lionel
+ * Date: 01/03/2016
+ * Time: 18:33
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\ChatGroup;
+use AppBundle\Entity\User;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+
+
+class LoadGroupFixtures extends AbstractFixture implements OrderedFixtureInterface
+{
+
+    function load(ObjectManager $manager)
+        {
+
+            $i = 1;
+
+            while($i <= 100) {
+
+                $chatGroup = new ChatGroup();
+                $chatGroup->setName("Groupe numero ".$i);
+                $chatGroup->addUser($this->getReference('user'));
+                $manager->persist($chatGroup);
+                $i++;
+            }
+            $manager->flush();
+        }
+
+    public function getOrder()
+    {
+        // the order in which fixtures will be loaded
+        // the lower the number, the sooner that this fixture is loaded
+        return 3;
+    }
+}
