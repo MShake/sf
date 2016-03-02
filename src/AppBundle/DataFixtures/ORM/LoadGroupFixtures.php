cf62cbdeef0cddc5f32091e86ba35c8e7b45f70a
Shake <fidalgo.antoine@gmail.com>
2016-03-02 16:47:18 +0100

    maj fixtures

--- src/AppBundle/DataFixtures/ORM/LoadGroupFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadGroupFixtures.php
@@ -15,14 +15,20 @@ use AppBundle\Entity\User;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 
 class LoadGroupFixtures extends AbstractFixture implements OrderedFixtureInterface {
-	
+
 	function load(ObjectManager $manager) {
+
+		$groupnames = array (
+				'Potes',
+				'ESGI',
+		);
+
 		$i = 0;
-		
+
 		while ( $i < 2 ) {
 			$chatGroup = new ChatGroup ();
-			$chatGroup->setName ( "Groupe numero " . ($i + 1) );
-			
+			$chatGroup->setName ($groupnames[$i]);
+
 			if ($i == 0) {
 				$chatGroup->addUser ( $this->getReference ( "users0" ) );
 				$chatGroup->addUser ( $this->getReference ( "users1" ) );
@@ -31,14 +37,14 @@ class LoadGroupFixtures extends AbstractFixture implements OrderedFixtureInterfa
 				$chatGroup->addUser ( $this->getReference ( "users0" ) );
 				$chatGroup->addUser ( $this->getReference ( "users3" ) );
 			}
-			
+
 			$this->addReference ( 'groups' . $i, $chatGroup );
 			$manager->persist ( $chatGroup );
 			$i ++;
 		}
 		$manager->flush ();
 	}
-	
+
 	public function getOrder() {
 		// the order in which fixtures will be loaded
 		// the lower the number, the sooner that this fixture is loaded
