02887927c3ddef6a5231de64afad15d073253f87
lionel95200x <lionel95200@gmail.com>
2016-03-02 12:33:46 +0100

    Register page

---
+++ src/UserBundle/Form/RegistrationType.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lionel
+ * Date: 02/03/2016
+ * Time: 12:26
+ */
+
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstname');
+        $builder->add('lastname');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
