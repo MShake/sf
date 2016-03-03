de14f0d0f5485b74d14c37493391c6187eb39e52
Florian <laforest.florian@gmail.com>
2016-03-03 12:32:49 +0100

    Admin - Liste et Modification de groupes

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -6,14 +6,70 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use AppBundle\Entity\Message;
+use AppBundle\Entity\ChatGroup;
+use AppBundle\Form\EditGroupForm;
+use AppBundle\Repository\UserRepository;
+use AppBundle\Repository\MessageRepository;
+use AppBundle\Repository\ChatGroupRepository;
 
-class AdminController extends Controller
-{
+class AdminController extends Controller{
+	
+	private $repoUser;
+	private $repoMessage;
+	private $repoChatGroup;
+	
 	/**
 	 * @Route("/admin", name="admin")
 	 * @Template()
 	 */
 	public function indexAction(Request $request){
 		
+	}
+	
+	/**
+	 * @Route("/admin/groups", name="admin-groups")
+	 * @Template()
+	 */
+	public function groupsAction(){
+		$this->initRepo();
+		$groups = $this->repoChatGroup->findAll();
+		
+		return array(
+				'groups' => $groups,
+		);
+	}
+	
+	/**
+	 * @Route("/admin/group/{id}", name="admin-group-edit")
+	 * @Template()
+	 */
+	public function editGroupAction(Request $request, $id){
+		$this->initRepo();
+		$group = $this->repoChatGroup->find($id);
+		$form = $this->createForm(EditGroupForm::Class, $group);
+		$form->handleRequest($request);
+		if($form->isSubmitted() && $form->isValid()){
+			$this->updateChatGroup($group);
+			return $this->redirect($this->generateUrl('admin-groups'));
+		}
+		
+		return array(
+				'form' => $form->createView(),
+				'group' => $group
+		);
+	}
+	
+	private function updateChatGroup(ChatGroup $chatGroup){
+		$em = $this->get('doctrine')->getManager();
+		$em->flush();
+		$this->get('session')->getFlashBag()->add('success', 'Groupe modifié');
+	}
+	
+	private function initRepo(){
+		$this->repoUser = $this->get('doctrine')->getManager()->getRepository('AppBundle:User');
+		$this->repoMessage = $this->get('doctrine')->getManager()->getRepository('AppBundle:Message');
+		$this->repoChatGroup = $this->get('doctrine')->getManager()->getRepository('AppBundle:ChatGroup');
 	}
 }
