580c29eb37ffe46bbdce8af4cfdf2d8bbec5df51
Shake <fidalgo.antoine@gmail.com>
2016-03-03 12:48:19 +0100

    reported msg hidden

--- src/AppBundle/Services/MessageService.php
+++ src/AppBundle/Services/MessageService.php
@@ -6,22 +6,22 @@ use AppBundle\Entity\Message;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\Response;
 
-class MessageService 
+class MessageService
 {
-    
+
     protected $em;
 
     public function __construct(EntityManager $em)
     {
         $this->em = $em;
     }
-    
+
     public function add($message_id)
     {
         $message = $this->em->getRepository('AppBundle:Message')->find($message_id);
-
+        
         $message->setReport(1);
         $this->em->flush();
     }
-    
+
 }
