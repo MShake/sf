c79987c930e6cfdb63e503e77516891ae45c608a
Florian <laforest.florian@gmail.com>
2016-03-04 13:23:39 +0100

    Add tests messageController

--- tests/AppBundle/Controller/MessageControllerTest.php
+++ tests/AppBundle/Controller/MessageControllerTest.php
@@ -4,21 +4,12 @@ namespace tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class MessageControllerTest extends WebTestCase
-{
-    public function testIndex()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
-        $form = $crawler->selectButton('submit')->form();
-
-        // set some values
-        $form['username'] = 'lbueno';
-        $form['password'] = 'lolochat';
-
-        $crawler = $client->submit($form);
-
-    }
+class MessageControllerTest extends WebTestCase {
+	public function testIndex() {
+		$client = static::createClient ();
+		
+		$crawler = $client->request ( 'GET', '/login' );
+		
+		$this->assertGreaterThan ( 0, $crawler->filter ( 'html:contains("Identifiant")' )->count () );
+	}
 }
