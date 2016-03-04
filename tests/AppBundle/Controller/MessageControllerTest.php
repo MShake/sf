15174be2bc92a81e5c7a4080622dd1c165cf3bef
Florian <laforest.florian@gmail.com>
2016-03-04 13:28:32 +0100

    add test

--- tests/AppBundle/Controller/MessageControllerTest.php
+++ tests/AppBundle/Controller/MessageControllerTest.php
@@ -12,4 +12,15 @@ class MessageControllerTest extends WebTestCase {
 		
 		$this->assertGreaterThan ( 0, $crawler->filter ( 'html:contains("Identifiant")' )->count () );
 	}
+	public function testAccesRegister() {
+		$client = static::createClient ();
+		
+		$crawler = $client->request ( 'GET', '/login' );
+		
+		$link = $crawler->filter ( 'a:contains("SIGN UP")' )->eq ( 1 )->link ();
+
+		$crawler = $client->click ( $link );
+		
+		$this->assertGreaterThan ( 0, $crawler->filter ( 'html:contains("Sign Up")' )->count () );
+	}
 }
